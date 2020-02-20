c31dad312ed7f634adf16eaf2c1fcb28ed32018f
dtodt <danieltodt@gmail.com>
2020-02-19 21:14:11 -0300

    refactor: state encapsulation done right

--- lib/models/tasks_state.dart
+++ lib/models/tasks_state.dart
@@ -1,18 +1,16 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:todoey/models/task.dart';
 
 class TasksState extends ChangeNotifier {
   final List<Task> _tasks = [];
 
-  List<Task> get tasks => _tasks;
+  UnmodifiableListView<Task> get tasks => _tasks;
 
-  int get allTasks {
-    return _tasks.length;
-  }
+  int get allTasks => _tasks.length;
 
-  int get unDoneTasks {
-    return _tasks.where((task) => !task.isDone).length;
-  }
+  int get unDoneTasks => _tasks.where((task) => !task.isDone).length;
 
   void addTask(String name) {
     _tasks.add(Task(name: name));
